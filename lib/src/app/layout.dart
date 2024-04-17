600bd4ca3a728d7a0c21fccf53d24276030f257f
etherealhero <gutcan4312@gmail.com>
2024-04-18 01:29:03 +1000

    form validation

--- lib/src/app/layout.dart
+++ lib/src/app/layout.dart
@@ -13,31 +13,3 @@ class Screen {
   final Widget? floatingActionButton;
   final FloatingActionButtonLocation? floatingActionButtonLocation;
 }
-
-showSnackBarFun(context, String text) {
-  SnackBar snackBar = SnackBar(
-    content: Row(
-      children: [
-        Icon(
-          Icons.info_outline_rounded,
-          color: Theme.of(context).colorScheme.onPrimary,
-        ),
-        const SizedBox(width: 10),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18,
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
-        ),
-      ],
-    ),
-    backgroundColor: Theme.of(context).colorScheme.primary,
-    dismissDirection: DismissDirection.horizontal,
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-        bottom: MediaQuery.of(context).size.height - 150, left: 10, right: 10),
-  );
-
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
--- lib/src/widgets/add_task_form.dart
+++ lib/src/widgets/add_task_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/src/app/layout.dart';
 import 'package:flutter_sandbox/src/models/task.dart';
 
 import '/src/app/repository.dart';
@@ -17,10 +16,11 @@ class TaskForm extends StatefulWidget {
 class _TaskFormState extends State<TaskForm> {
   final repository = Repository();
 
+  final _formKey = GlobalKey<FormState>();
+  late FocusNode _titleFocusNode;
+
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-
-  late FocusNode _titleFocusNode;
 
   @override
   void initState() {
@@ -47,74 +47,85 @@ class _TaskFormState extends State<TaskForm> {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(24.0),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(widget.title, style: const TextStyle(fontSize: 20)),
-            const SizedBox(height: 10),
-            TextFormField(
-              autofocus: true,
-              focusNode: _titleFocusNode,
-              controller: _titleController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Title',
-              ),
-            ),
-            const SizedBox(height: 15),
-            TextFormField(
-              controller: _descriptionController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Description',
-              ),
-              maxLines: 3,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton.icon(
-                  icon: Icon(Icons.close, color: Colors.red.shade300),
-                  label: Text('Cancel',
-                      style: TextStyle(color: Colors.red.shade300)),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+        child: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(widget.title, style: const TextStyle(fontSize: 20)),
+              const SizedBox(height: 10),
+              TextFormField(
+                autofocus: true,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Title shouldn't be empty";
+                  }
+                  return null;
+                },
+                focusNode: _titleFocusNode,
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Title',
                 ),
-                ElevatedButton.icon(
-                  icon: Icon(Icons.save, color: Colors.white.withOpacity(0.9)),
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  label: Text('Save',
-                      style: TextStyle(color: Colors.white.withOpacity(0.9))),
-                  onPressed: () async {
-                    if (_titleController.text.trim().isEmpty) {
-                      showSnackBarFun(context, "Title shouldn't be empty");
-                      _titleFocusNode.requestFocus();
-                      return;
-                    }
-
-                    if (widget.task != null) {
-                      widget.task!.title = _titleController.text;
-                      widget.task!.description = _descriptionController.text;
-
-                      await repository.saveTask(widget.task!);
-                    } else {
-                      await repository.saveTask(Task(
-                        title: _titleController.text,
-                        description: _descriptionController.text,
-                      ));
-                    }
-
-                    if (!context.mounted) return;
-
-                    Navigator.pop(context);
-                  },
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
                 ),
-              ],
-            )
-          ],
+                maxLines: 3,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton.icon(
+                    icon: Icon(Icons.close, color: Colors.red.shade300),
+                    label: Text('Cancel',
+                        style: TextStyle(color: Colors.red.shade300)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ElevatedButton.icon(
+                    icon:
+                        Icon(Icons.save, color: Colors.white.withOpacity(0.9)),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    label: Text('Save',
+                        style: TextStyle(color: Colors.white.withOpacity(0.9))),
+                    onPressed: () async {
+                      if (!_formKey.currentState!.validate()) {
+                        _titleController.text = "";
+                        _titleFocusNode.requestFocus();
+                        return;
+                      }
+
+                      if (widget.task != null) {
+                        widget.task!.title = _titleController.text.trim();
+                        widget.task!.description = _descriptionController.text;
+
+                        await repository.saveTask(widget.task!);
+                      } else {
+                        await repository.saveTask(Task(
+                          title: _titleController.text.trim(),
+                          description: _descriptionController.text,
+                        ));
+                      }
+
+                      if (!context.mounted) return;
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
